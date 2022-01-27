192043b77551c725bee8bf2ed2c6e175fdcb21aa
Joel <78717119+code-monarch@users.noreply.github.com>
2022-01-27 22:28:06 +0100

    Update whitepaper.jsx

--- pages/whitepaper.jsx
+++ pages/whitepaper.jsx
@@ -3,16 +3,17 @@ import Head from "next/head";
 import styles from "../styles/whitepaper.module.css";
 
 const whitepaper = () => {
-  return 
-   <div className={styles.container}>
-     <Head>
-       <title>Metasino-the first truly decentralized Casino</title>
-       <meta name='description' content='Metasino Whitepaper' />
-       {/* <link rel='icon' href='/MetasinoLogo.ico' /> */}
-       <link rel='shortcut icon' href='/MetasinoLogo.ico' />
-     </Head>
-     <h2 className={styles.h2}>WHITEPAPER IS CURRENTLY BEING UPDATED</h2>
-   </div>;
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Metasino-the first truly decentralized Casino</title>
+        <meta name='description' content='Metasino Whitepaper' />
+        {/* <link rel='icon' href='/MetasinoLogo.ico' /> */}
+        <link rel='shortcut icon' href='/MetasinoLogo.ico' />
+      </Head>
+      <h2 className={styles.h2}>WHITEPAPER IS CURRENTLY BEING UPDATED</h2>
+    </div>
+  );
 };
 
 export default whitepaper;
