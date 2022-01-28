2d5f60227e75460e212899625ae1073ae75f5163
Joel <78717119+code-monarch@users.noreply.github.com>
2022-01-28 22:09:48 +0100

    .hj.klhj

--- components/Tokenomics.jsx
+++ components/Tokenomics.jsx
@@ -1,7 +1,5 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from "react";
 import styles from "./tokenomics.module.css";
-
-import { Flex, VStack, Box, Text } from "@chakra-ui/react";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -9,7 +7,7 @@ import "aos/dist/aos.css";
 const isServer = typeof window === "undefined";
 const WOW = !isServer ? require("wow.js") : null;
 
-const Tokenomics = ({id}) => {
+const Tokenomics = ({ id }) => {
   // INSTANTIATE WOW JS
   useEffect(() => {
     const wow = new WOW().init();
@@ -25,20 +23,6 @@ const Tokenomics = ({id}) => {
         alt=''
       />
       <h3>Total supply : 2,000,000,000</h3>
-      {/* <VStack marginBottom={10} color='#fff'>
-        <Flex>
-          <Text>
-            Private sale: 1BNB = 1,212,500 $casino token <br /> 2BNB = 2,425,000
-            $casino token
-          </Text>
-        </Flex>
-        <Flex>
-          <Text>
-            Public sale: 1BNB = 732,075 $casino token <br /> 2BNB = 1,464,150
-            $casino token
-          </Text>
-        </Flex>
-      </VStack> */}
       <div className={styles.row}>
         <div
           className={`${styles.card1} ${
@@ -59,7 +43,8 @@ const Tokenomics = ({id}) => {
           <h3>Staking/Rewards</h3>
           <p>
             yield-bearing assets enabling you to earn revenue through fees
-            collected from players.Imagine owning a Poker table, Slot machine in Las Vegas
+            collected from players.Imagine owning a Poker table, Slot machine in
+            Las Vegas
           </p>
         </div>
         {/* THIRD CARD */}
@@ -70,29 +55,18 @@ const Tokenomics = ({id}) => {
           data-wow-delay='370ms'>
           <h3>Staking</h3>
           <p>
-            {" "}
             Metasino will have a single-asset staking for passive income
             generation. Yield farming will come later on as Metasino development
             progresses.
           </p>
         </div>
-        {/* FOURTH CARD
-        <div
-          className={`${styles.card4} ${
-            styles.packageCards
-          }  ${"wow animate__animated animate__flipInY"}`}
-          data-wow-delay='170ms'>
-          <h3>Team/Advisors</h3>
-          <p> 10%</p>
-          <p>200,000,000</p>
-        </div> */}
-        {/* FIFTH CARD */}
+
         <div
           className={`${styles.card5} ${
             styles.packageCards
           }  ${"wow animate__animated animate__flipInY"}`}
           data-wow-delay='370ms'>
-          <h3>Social life & Gambling</h3>
+          <h3>Social life and Gambling</h3>
           <p>
             virtual nightclub packed with stripclub, VIP lounge and a cocktail
             bar, and a virtual social park where individuals can meet, interact
@@ -115,6 +89,6 @@ const Tokenomics = ({id}) => {
       </div>
     </section>
   );
-}
+};
 
-export default Tokenomics
+export default Tokenomics;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,8 @@
 import '../styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
-import theme from "../theme/index";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
-    </ChakraProvider>
   );
 }
 
