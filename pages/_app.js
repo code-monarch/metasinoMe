acb4ee920179c9b026bffe1a33e6b38bb4696916
Joel <78717119+code-monarch@users.noreply.github.com>
2022-02-06 14:40:54 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,11 @@
 import React from "react";
-// import { useRouter } from "next/router";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  // const router = useRouter();
 
 
-  // STATE FOR SHOWING PRELOADER
-//   const [loading, setLoading] = useState(false);
-  
-// useEffect(() => {
-//   const handleStart = (url) => {
-//     url !== router.pathname ? setLoading(true) : setLoading(false);
-//   };
-//   const handleComplete = (url) => setLoading(false);
 
-//   router.events.on("routeChangeStart", handleStart);
-//   router.events.on("routeChangeComplete", handleComplete);
-//   router.events.on("routeChangeError", handleComplete);
-// }, [router]);
-
-
-  return
-      <Component {...pageProps} />
+  return <Component {...pageProps} />
 }
 
 export default MyApp;
